7666e8d37bee386c13c4d6bad8a461886a6550e1
Are Fosså <are.fossaa@gmail.com>
2026-01-13 17:20:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 
 ingredients_list = st.multiselect(
@@ -28,7 +28,7 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5
 )
-if ingredients_list :
+if ingredients_list:
 
     ingredients_string = ''
 
@@ -57,6 +57,7 @@ if ingredients_list :
     
 
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
 
 
 
